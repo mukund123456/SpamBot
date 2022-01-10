aa76e9c4a49f7f1ce27510bf8b951da64eb0ab07
LEGEND_MUKUND <96979426+mukund123456@users.noreply.github.com>
2022-01-10 19:50:32 +0530

    Update __main__.py

--- RiZoeLXSpam/__main__.py
+++ RiZoeLXSpam/__main__.py
@@ -22,8 +22,8 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("RiZoeL Bot Spam Successfully deployed -!")
-print("Enjoy! Do visit @RiZoeLX")
+print("Alien Bot Spam Successfully deployed -!")
+print("Enjoy! Do visit @Alienxsupport")
 
 if __name__ == "__main__":
     Riz.run_until_disconnected()
